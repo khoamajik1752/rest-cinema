3bbaeaa554d92fbd5988129c077654c799356f10
tian <khoanguyen517@gmail.com>
2023-09-21 00:12:35 +0700

    add security

---
+++ auth.py
@@ -0,0 +1,42 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing import Annotated
+from config import Config
+from model import UserModel
+from schemas import UserLogin
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=Config.TOKEN_URL)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, Config.SECRET_KEY, algorithms=[Config.ALGORITHM])
+        print(payload)
+        username: str = payload.get("account")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user= UserModel.find_one({
+        "username":username
+    })
+
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[UserLogin, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
